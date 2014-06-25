bf3f9ae097839805aed744a61ad1ac7c024e6bcf
bandhavivinay <Tavant3@BANDHAVIVINAY-MAC.local>
2014-06-25 12:46:41 +0530

    new structure

---
+++ TestJenkins/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  TestJenkins
+//
+//  Created by Bandhavi on 25/06/14.
+//  Copyright (c) 2014 Tavant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSString *unusedString1 = @"abc";
+    NSString *unusedString2 = @"abc";
+    NSString *newString = @"test";
+    //change
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
